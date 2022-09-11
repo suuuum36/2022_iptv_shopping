f1610fdbcde578f4f91311e1e11290c14ffb6972
이수민 <sumin@isumins-MacBook-Air.local>
2022-09-11 11:47:28 +0900

    채널 네비게이터

--- src/channel.js
+++ src/channel.js
@@ -1,10 +1,29 @@
 import './css/channel.css';
 import {SectionLine} from './navigator';
- 
+
+function DateNavigator ({day, date, line}) {
+  return (
+    <div className='date_wrapper'>
+      <p>{day}</p>
+      <p className='date_text'><span>{date}</span> 일</p>  
+      {line ? <div className='date_line'></div> : null}
+    </div>
+  )
+}
+
+const dateArray = [
+  {day: '오늘', date: '29', line:'y'},
+  {day: '내일', date: '30', line:'y'},
+  {day: '수요일', date: '31', line:'y'},
+  {day: '목요일', date: '1'}
+]
+
 function Channel() {
   return (
     <section className='channel_wrapper'>
-      <div className='date_gnb'></div>
+      <div className='date_gnb'>
+        {dateArray.map(element => (<DateNavigator day={element.day} date={element.date} line={element.line}/>))}
+      </div>
       <div className='list_wrapper'></div>
     </section>
   );
--- src/navigator.js
+++ src/navigator.js
@@ -3,10 +3,10 @@ import ImgMenu from './img/nav/ic_menu.svg';
 import ImgSearch from './img/nav/ic_search.svg';
 import ImgNavBtn1 from './img/nav/ic_nav_btn_1.svg';
 
-function FunctionButton (props) {
+function FunctionButton ({name, imgUrl}) {
     return (
         <div className="navigator_left_btn">
-            <img src={props.imgUrl}/>
+            <img src={imgUrl} alt={name}/>
         </div>
     );
 }
@@ -20,30 +20,34 @@ function LabelButton ({label, imgUrl, focus}) {
   );
 }
 
-function SectionLine (props) {
+function SectionLine ({height}) {
   return (
-    <div className='section_line' style={{height: `${props.height}`}}></div>
+    <div className='section_line' style={{height: `${height}`}}></div>
   )
 }
 
+const FunctionArray = [
+  {id:'func_btn_1', name:'메뉴', imgUrl: ImgMenu},
+  {id:'func_btn_2', name:'검색', imgUrl: ImgSearch}
+]
+
 const labelArray = [
-  {id:1, name: '지금하는 방송', img:ImgNavBtn1},
-  {id:2, name: '베스트'},
-  {id:3, name: 'U+ 추천'},
-  {id:4, name: '홈쇼핑 편성표', focus:'active'},
-  {id:5, name: '편성메뉴'}
+  {id:'label_btn_1', name: '지금하는 방송', img: ImgNavBtn1},
+  {id:'label_btn_2', name: '베스트'},
+  {id:'label_btn_3', name: 'U+ 추천'},
+  {id:'label_btn_4', name: '홈쇼핑 편성표', focus:'active'},
+  {id:'label_btn_5', name: '편성메뉴'}
 ];
 
 function Navigator (props) {
   return (
     <section className='menu_navigator'>
       <div className='left_gnb'>
-          <FunctionButton imgUrl={ImgMenu}/>
-          <FunctionButton imgUrl={ImgSearch}/>
+          {FunctionArray.map(element=>(<FunctionButton key={element.id} name={element.name} imgUrl={element.imgUrl}/>))}
       </div>
       <SectionLine height='60px'/>
       <div className='right_gnb'>
-        {labelArray.map(label=>(<LabelButton key={label.id} label={label.name} imgUrl={label.img} focus={label.focus}/>))}
+        {labelArray.map(element=>(<LabelButton key={element.id} label={element.name} imgUrl={element.img} focus={element.focus}/>))}
       </div>  
     </section>
   );
